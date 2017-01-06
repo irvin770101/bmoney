8cb44304329a4645d990bfe24ce89e9a8f818b75
irvin <chuang.f.c@gmail.com>
2017-01-06 17:06:14 +0800

    irvin
    新增會員資料表

--- database/migrations/2017_01_06_080713_create_member_table.php
+++ database/migrations/2017_01_06_080713_create_member_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMemberTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('member', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nick_name',255)->comment('暱稱');
+            $table->string('account',255)->comment('帳號');
+            $table->string('password',255)->comment('密碼');
+            $table->string('email',255)->unique()->comment('電子信箱');
             $table->timestamps();
         });
     }
